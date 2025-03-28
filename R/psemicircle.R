e9b7883a912a34add399da7ff63a0c0f0bcdc098
Marcus Quincy <marcus@marcusquincy.org>
2025-03-27 20:53:28 -0600

    implement psemicircle

--- R/psemicircle.R
+++ R/psemicircle.R
@@ -1,3 +1,18 @@
 psemicircle <- function(x, R) {
-  return("hehe")
+  if (R <= 0) {
+    stop("R must be positive.")
+  }
+
+  result <- numeric(length(x))
+
+  for (i in 1:length(x)) {
+    val <- x[i]
+    if (abs(val) > R) {
+      stop("x must be within radius.")
+    }
+
+    result[i] <- 0.5 + (val * sqrt(R^2 - val^2)) / (pi * R^2) + asin(val/R)/pi
+  }
+
+  return(result)
 }
