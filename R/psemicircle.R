9d93badd1db52e0c2c14d226cc5c304a09168927
Marcus Quincy <marcus@marcusquincy.org>
2025-03-27 20:57:47 -0600

    Documentation for psemicircle

--- R/psemicircle.R
+++ R/psemicircle.R
@@ -1,3 +1,18 @@
+#' Semicircle Distribution Cumulative Density Function (CDF)
+#'
+#' This function computes the cumulative density function (CDF) of the semicircle distribution with a given radius \( R \).
+#'
+#' @param x A numeric value representing the point at which the CDF is evaluated. It must lie in the range \([-R, R]\).
+#' @param R A positive numeric value representing the radius parameter of the semicircle distribution.
+#'
+#' @return A numeric value representing the CDF of the semicircle distribution at point `x`.
+#'
+#' @examples
+#' psemicircle(0, 1)   # Should return 0.5
+#' psemicircle(-1, 1)  # Should return 0
+#' psemicircle(1.5, 1) # Should return 0 (outside the range)
+#'
+#' @export
 psemicircle <- function(x, R) {
   if (R <= 0) {
     stop("R must be positive.")
