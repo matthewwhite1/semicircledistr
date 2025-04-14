562d67bcee02bdc8189daeb8128e55ccc221457e
Marcus Quincy <marcus@marcusquincy.org>
2025-04-14 15:41:18 -0600

    Update qq plot for rsemicircle to be better

--- Scripts/QQ_plot.R
+++ Scripts/QQ_plot.R
@@ -2,13 +2,16 @@
 ## This script generates a QQ plot comparing rsemicircle with qsemicircle
 ################################################################################
 
-jpeg("Plots/QQ_plot.jpeg", width = 600, height = 600)
-
-par(mfrow = c(3, 2))
-for (i in seq(.5,3,.5)) {
+## par(mfrow = c(3, 2))
+## radii <- seq(.5,3,.5)
+par(mfrow = c(2, 1))
+radii <- c(0.75, 2)
+for (i in radii) {
   random <- sort(rsemicircle(1000, i))
   theoretical <- qsemicircle(ppoints(1000), i)
-  qqplot(random, theoretical, main=paste("QQ Plot of random results vs theoretical for R = ", i))
+  qqplot(random, theoretical, main=paste("QQ Plot of rsemicircle vs theoretical for R = ", i), xlab="rsemicircle", ylab="Theoretical", cex=2)
+  abline(0,1, col="red", lwd=2)
 }
 
-dev.off()
+## Uncomment to export image
+## dev.print(png, "rsemicircle-qq.png", width=720)
